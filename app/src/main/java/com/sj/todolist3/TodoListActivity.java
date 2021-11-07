f45a2fe983ceef361568e21be29c14f103313360
Zheng Ji Tan <dunlike2big@hotmail.com>
2021-11-06 23:25:51 -0400

    todolist halfdone

--- app/src/main/java/com/sj/todolist3/TodoListActivity.java
+++ app/src/main/java/com/sj/todolist3/TodoListActivity.java
@@ -1,6 +1,10 @@
 package com.sj.todolist3;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,6 +13,22 @@ public class TodoListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_todolist);
+        ListView todoListView = (ListView)findViewById(R.id.thingsToDo);
+
+    }
+
+    public void createTask(ListView todoListView){
+        TextView field = (TextView)findViewById(R.id.createTask);
+        field.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
+
+                }
+                return false;
+            }
+        });
     }
 }
