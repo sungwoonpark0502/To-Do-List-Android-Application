89b0722cae6ffbc3146c66aa9ad8b72e91c8e81f
sooyoung <hasu0907*>
2021-11-06 20:59:00 -0400

    initial commit

--- app/src/main/java/com/sj/todolist3/SignupActivity.java
+++ app/src/main/java/com/sj/todolist3/SignupActivity.java
@@ -1,14 +1,14 @@
 package com.sj.todolist3;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
---
+++ app/src/main/java/com/sj/todolist3/TodoListActivity.java
@@ -0,0 +1,14 @@
+package com.sj.todolist3;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TodoListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
