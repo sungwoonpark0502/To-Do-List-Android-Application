5f8d955a5d358f852fe50c83dfc36f1fa99be097
Zheng Ji Tan <dunlike2big@hotmail.com>
2021-11-07 01:01:49 -0500

    added signupactivity

--- app/src/main/java/com/sj/todolist3/TodoListActivity.java
+++ app/src/main/java/com/sj/todolist3/TodoListActivity.java
@@ -1,10 +1,6 @@
 package com.sj.todolist3;
 
 import android.os.Bundle;
-import android.view.KeyEvent;
-import android.view.View;
-import android.widget.ListView;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,21 +10,21 @@ public class TodoListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todolist);
-        ListView todoListView = (ListView)findViewById(R.id.thingsToDo);
+//        ListView todoListView = (ListView)findViewById(R.id.thingsToDo);
 
     }
 
-    public void createTask(ListView todoListView){
-        TextView field = (TextView)findViewById(R.id.createTask);
-        field.setOnKeyListener(new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View view, int keyCode, KeyEvent event) {
-                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
-                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
-
-                }
-                return false;
-            }
-        });
-    }
+//    public void createTask(ListView todoListView){
+//        TextView field = (TextView)findViewById(R.id.createTask);
+//        field.setOnKeyListener(new View.OnKeyListener() {
+//            @Override
+//            public boolean onKey(View view, int keyCode, KeyEvent event) {
+//                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+//                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
+//
+//                }
+//                return false;
+//            }
+//        });
+//    }
 }
